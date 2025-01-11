84b9c0d9a8dbbd37485cb6cad18bab42401b60bc
tommasovenza <tommasovenza@gmail.com>
2025-01-11 16:16:41 +0100

    creating a new seeder and updating job_listings table with new values for tags column

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $data_tags = [
+            ["tags" => "development,coding,java,python"],
+            ["tags" => "marketing,advertising"],
+            ["tags" => "web development,programming"],
+            ["tags" => "data analysis,statistics"],
+            ["tags" => "graphic design,creative"],
+            ["tags" => "data science,machine learning"],
+            ["tags" => "UX design,user research"],
+            ["tags" => "digital marketing,SEO,analytics"],
+            ["tags" => "product management,tech"],
+            ["tags" => "IT support,networking"],
+        ];
+
+        // getting jobs from DB
+        $jobs = DB::table('job_listings')->get();
+
+        // Updating jobs 
+        foreach ($jobs as $index => $job) {
+            DB::table('job_listings')->select('tags')
+                ->where('id', '=', $job->id)
+                ->update([
+                    'tags' => $data_tags[$index]["tags"]
+                ]);
+        }
+    }
+}
--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -2,7 +2,7 @@
 <x-layout>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
     @foreach ($listings as $listing)
-        <x-job-card :job="$listing"></x-job-card>
+        <x-job-card :job="$listing" />
     @endforeach
     </div>
 </x-layout>
